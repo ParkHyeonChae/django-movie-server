ecc099065d54d3c39053838ed91a3a40edf16100
ParkHyeonChae <park19996@naver.com>
2020-07-25 14:26:12 +0900

    TestCase1 작성 (영화 리스트 10개 출력, type 확인)

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+"""
+DB Field : title, year, rating, genres, summary
 
-# Create your tests here.
+# sort_by : String (title, year, rating, peers, seeds, download_count, like_count, date_added)
+# order_by : String (desc, asc)
+# limit : Integer between 1 - 50 (inclusive)
+"""
+
+from django.test import TestCase
+import requests
+import json
+
+
+# TESTCASE 1 - 영화정보 다운로드순 10개
+api_url = 'https://yts.mx/api/v2/list_movies.json?sort_by=download_count&limit=10'
+data = requests.get(api_url)
+json_data = json.loads(data.text)
+
+for data in json_data['data']['movies']:
+    print(
+        '제목 : ', data['title'], type(data['title']),
+        '\n년도 : ', data['year'], type(data['year']),
+        '\n평점 : ', data['rating'], type(data['rating']),
+        '\n장르 : ', data['genres'], type(data['genres']),
+        '\n개요 : ', data['summary'], type(data['summary']), '\n\n'
+    )
