904ad985110697c42113d2e5877b99651d49a515
ParkHyeonChae <park19996@naver.com>
2020-07-25 18:32:33 +0900

    sort_by parameter JSON 응답 구현

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+"""api/views.py"""
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import MovieList
+
+
+def all_list_view(request):
+    queryset = MovieList.objects.values().order_by('id')[:20]
+
+    # /api?sort_by={parameter}
+    sort_by = request.GET.get('sort_by', '')
+
+    if sort_by:
+        if sort_by == 'title':
+            queryset = MovieList.objects.values().order_by('title')[:20]
+        elif sort_by == 'year':
+            queryset = MovieList.objects.values().order_by('-year')[:20]
+        elif sort_by == 'rating':
+            queryset = MovieList.objects.values().order_by('-rating')[:20]
+
+       
+    data = list(queryset)
+
+    return JsonResponse(data, safe=False)
