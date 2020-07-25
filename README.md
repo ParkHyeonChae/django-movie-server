cf57cb14b871c83151d613643c66b1198c17ac7d
ParkHyeonChae <park19996@naver.com>
2020-07-26 01:47:32 +0900

    주석처리, sample index페이지 구현

--- README.md
+++ README.md
@@ -20,6 +20,11 @@ $ pip install -r requirements.txt
 $ python manage.py makemigrations
 $ python manage.py migrate
 ```
+### CreateSuperUser
+
+```
+$ python manage.py createsuperuser
+```
 
 ### Runserver
 
--- api/views.py
+++ api/views.py
@@ -12,19 +12,23 @@ from django.views import View
 
 
 class MovieListApi(View):
+    # GET요청이 들어올 시 접근
     def get(self, request):
+        # Parameter 초기값
         start = 0
         last = 20
         page = 1
         sort_by = 'id'
         genre = ''
 
+        # GET요청으로 들어온 URL Parameter 저장
         get_limit = request.GET.get('limit', '')
         get_page = request.GET.get('page', '')
         get_sort_by = request.GET.get('sort_by', '')
         get_genre = request.GET.get('genre', '')
         get_order_by = request.GET.get('order_by', '')
 
+        # limit Parameter
         if get_limit:
             if int(get_limit) >= 1 and int(get_limit) <= 50:
                 last = int(get_limit)
@@ -33,6 +37,7 @@ class MovieListApi(View):
         else:
             get_limit = 20
             
+        # page Parameter (페이지당 limit갯수를 반환하는 로직)
         if get_page:
             if int(get_page) >= 1 and int(get_page) * last <= 500:
                 start = (last * int(get_page)) - last
@@ -42,6 +47,7 @@ class MovieListApi(View):
         else:
             get_page = 1
 
+        # sort_by Parameter
         if get_sort_by:
             if get_sort_by == 'title':
                 sort_by = 'title'
@@ -50,9 +56,11 @@ class MovieListApi(View):
             elif get_sort_by == 'rating':
                 sort_by = '-rating'
 
+        # genre Parameter
         if get_genre:
             genre = get_genre
 
+        # order_by Parameter
         if get_order_by:
             if get_order_by == 'asc':
                 if sort_by[0] != '-':
@@ -63,8 +71,9 @@ class MovieListApi(View):
                 else:
                     sort_by = sort_by[1:]
 
-
+        # json Response에 출력될 movie 갯수
         movie_count = MovieList.objects.values().filter(genre__icontains=genre).order_by(sort_by).count()
+        # json Response에 출력될 movie data
         queryset = MovieList.objects.values().filter(genre__icontains=genre).order_by(sort_by)[start:last]
         data = list(queryset)
 
@@ -85,15 +94,21 @@ class MovieListApi(View):
 
     # postman 사용 POST 요청시 header에 X-CSRFToken : zbJi6TjifNkMPWXFalus0YXNquCqwQkeNz7g3Oy8XIm9LDpF0MB8lUg71EKJLG9J 추가
     def post(self, request):
+        # POST요청이 들어올 시 URL Parameter 저장
         post_title = request.GET.get("title")
         post_year = request.GET.get('year')
         post_rating = request.GET.get('rating')
         post_genres = request.GET.get('genres')
         post_summary = request.GET.get('summary')
 
+        # 각 Parameter는 필수요소이므로 확인 후 DB에 저장
         if post_title and post_year and post_rating and post_genres and post_summary:
             MovieList.objects.create(title=post_title, year=post_year, rating=post_rating, genre=post_genres, summary=post_summary)
-            
+
             return HttpResponse("Query was successful")
         else:
             return HttpResponse("Please enter all fields...")
+
+    
+def index(request):
+    return render(request, 'api/index.html')
